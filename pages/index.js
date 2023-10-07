df644b9c708b7c21b5ea1ef9f51e787112624fd9
Mikulas Richter <mikulasrichter@gmail.com>
2023-10-07 17:48:15 +0200

    add queries to the pdodructs page

--- pages/index.js
+++ pages/index.js
@@ -30,8 +30,47 @@
 
 import React from "react";
 import { Hero, ProductSection, Description, Layout } from "../components";
+import { productsData } from "../data";
 
-const HomePage = () => {
+export async function getServerSideProps({ params, query }) {
+  console.log({ params, query });
+  // page i get from query
+  const page = query.page ?? 1;
+  let limit = 6;
+  if (query.limit && query.limit >= 6 && query.limit <= 12) {
+    limit = query.limit;
+  }
+  const sortKeys = ["price", "title", "createdAt"];
+  let sortKeyString = "createdAt";
+  if (query.sortKey && sortKeys.includes(query.sortKey)) {
+    sortKeyString = query.sortKey;
+  }
+  const skip = (page - 1) * limit;
+  const newProducts = productsData.slice(skip, page * limit);
+
+  return {
+    props: {
+      count: productsData.length,
+      page,
+      limit,
+      skip,
+      sortKey: sortKeyString,
+      products: newProducts,
+    },
+  };
+}
+
+const HomePage = (props) => {
+  const { count, page, limit, skip, sortKey, products } = props;
+  console.log({
+    count,
+    page,
+    limit,
+    skip,
+    sortKey,
+  });
+  // const { products, sayHi } = props;
+
   return (
     <main className="mx-auto max-w-screen-xl md:h-full">
       <section className="">
@@ -41,7 +80,7 @@ const HomePage = () => {
         <Description />
       </section>
       <section className="">
-        <ProductSection />
+        <ProductSection products={props.products} />
       </section>
     </main>
   );
