c5aa5ce77530294d0d6607e0124e35cafd838fed
Mikulas Richter <mikulasrichter@gmail.com>
2023-10-12 15:58:37 +0200

    moved images

---
+++ public/pictures/products/index.ts
@@ -0,0 +1,37 @@
+import people1 from "./people1.png";
+import people2 from "./people2.png";
+import premium1 from "./premium1.png";
+import premium2 from "./premium2.png";
+import pets1 from "./pets1.png";
+import pets2 from "./pets2.png";
+import food1 from "./food1.png";
+import food2 from "./food2.png";
+import food3 from "./food3.png";
+import cities1 from "./cities1.png";
+import cities2 from "./cities2.png";
+import cities3 from "./cities3.png";
+import nature1 from "./nature1.png";
+import nature2 from "./nature2.png";
+import dog from "./dog.png";
+import landmOne from "./landmOne.png";
+import landmTwo from "./landmTwo.png";
+
+export {
+  people1,
+  people2,
+  premium1,
+  premium2,
+  dog,
+  pets1,
+  pets2,
+  food1,
+  food2,
+  food3,
+  landmOne,
+  landmTwo,
+  cities1,
+  cities2,
+  cities3,
+  nature1,
+  nature2,
+};
